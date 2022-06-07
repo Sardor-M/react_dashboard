26d9196084cfb8f3c5e2efc5e11f776f9312f660
Sardor Madaminov <sardor0968@gmail.com>
2022-06-07 23:14:18 +0900

    navbar components updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import className from "classnames";
 import ProTypes from "pro-types";
-import { Navbar as BSNavbar, Container } from "reactstrap";
+import { Navbar as NavbarBs, Container } from "reactstrap";
 import { colors } from "@material-ui/core";
 import shadows from "@material-ui/core/styles/shadows";
 
@@ -10,3 +10,21 @@ const Navbar = ({ themed, fluid, className, color, ...otherProps }) => {
     "navbar-themed": themed || !!color,
   });
 };
+<NavbarBs className="navbarClass">
+  {""}
+  <Container className="navbar_wrap"></Container>
+</NavbarBs>;
+
+Navbar.proTypes = {
+  themed: ProTypes.bool,
+  fluid: ProTypes.bool,
+  className: ProTypes.string,
+  color: ProTypes.string,
+};
+
+Navbar.defaultProps = {
+  fluid: false,
+  themed: false,
+};
+
+export { Navbar };
