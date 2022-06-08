e45dd3ca38fe7edabd4268e76338ed72f7a77133
Sardor Madaminov <sardor0968@gmail.com>
2022-06-08 22:39:36 +0900

    sidebar close added

---
+++ src/components/Sidebar/SidebarClose.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const SidebarClose = (props) => {
+  <div className="sidebar_close">{props.children}</div>;
+
+  SidebarClose.propTypes = {
+    children: PropTypes.node,
+  };
+};
+export default SidebarClose;
