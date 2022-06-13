745d1c8c2691302cd0e0366784ce4bd5937fd238
Sardor Madaminov <sardor0968@gmail.com>
2022-06-13 23:24:59 +0900

    media body added

--- src/layout/NavbarMessages.js
+++ src/layout/NavbarMessages.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "pprop-types";
 import _ from "lodash";
+import { Avatar } from "@material-ui/core";
 
 const messageColor = ["text-success", "text-danger", "text-warning"];\
 const NavbarMessages = (props) => {
@@ -17,9 +18,31 @@ const NavbarMessages = (props) => {
                     <i className="fa  fa-pencil"/>
                 </ExtendedDropDown.Link>
             </ExtendedDropDown.Section>
+
+            <ExtendedDropDown.Section list>
+                <ListGroup>
+                    {<Media>
+                        <Media left>
+                            <Avatar.image src = {faker.image.avatar()}
+                            className="avatar-1"/>
+                        </Media>
+                        <Media body>
+                            <span className="d-flex justify-content-start">
+                                <i className={`fa fa-circle small ${messagesColors[index]} mr-2 d-flex align-items-center`}/>
+                            </span>
+                        </Media>
+                    </Media>  
+                    }
+                </ListGroup>
+
+            </ExtendedDropDown.Section>
         </ExtendedDropDown>
     </UncontrolledDropdown >
 
 }
+NavbarMessages.propTypes = {
+    className: PropTypes.string,
+    style: PropTypes.object
+};
 
-export default NavbarMessages;
+export {NavbarMessages};
