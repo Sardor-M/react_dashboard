863b7ba434c38537e31e2dc88b0244c9e0934e6a
Sardor Madaminov <sardor0968@gmail.com>
2022-06-13 23:19:05 +0900

    components updated in NavbarMessage

--- src/layout/NavbarMessages.js
+++ src/layout/NavbarMessages.js
@@ -2,8 +2,24 @@ import React from "react";
 import PropTypes from "pprop-types";
 import _ from "lodash";
 
-function NavbarMessages() {
-  return <div>NavbarMessages</div>;
+const messageColor = ["text-success", "text-danger", "text-warning"];\
+const NavbarMessages = (props) => {
+    <UncontrolledDropdown nav inNavbar {...props}>
+        <DropToggle nav>
+            <IconWithBadge badge ={<Badge pill color="secondary">111</Badge>}>
+                <i className="fa fa-envelope-o fa-fw"/>
+                </IconWithBadge>
+        </DropToggle>
+        <ExtendedDropDown right>
+            <ExtendedDropDown.Section className = "d-flex justify-content-between align-items-center">
+                <h4 className="m0"> Message</h4>
+                <ExtendedDropDown.Link to ="app/email">
+                    <i className="fa  fa-pencil"/>
+                </ExtendedDropDown.Link>
+            </ExtendedDropDown.Section>
+        </ExtendedDropDown>
+    </UncontrolledDropdown >
+
 }
 
 export default NavbarMessages;
