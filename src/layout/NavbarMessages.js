d704ac4f6766c501c8c392ad6248c5745929169e
Sardor Madaminov <sardor0968@gmail.com>
2022-06-14 17:12:25 +0900

    import fixed

--- src/layout/NavbarMessages.js
+++ src/layout/NavbarMessages.js
@@ -2,47 +2,66 @@ import React from "react";
 import PropTypes from "pprop-types";
 import _ from "lodash";
 import { Avatar } from "@material-ui/core";
+import {
+  Avatar,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Bagde,
+  ExtendedDropDown,
+  ListGroup,
+  ListGroupItem,
+  Media,
+  InputGroup,
+  inputGroupAdd,
+  Button,
+} from "./components";
 
-const messageColor = ["text-success", "text-danger", "text-warning"];\
+const messageColor = ["text-success", "text-danger", "text-warning"];
 const NavbarMessages = (props) => {
-    <UncontrolledDropdown nav inNavbar {...props}>
-        <DropToggle nav>
-            <IconWithBadge badge ={<Badge pill color="secondary">111</Badge>}>
-                <i className="fa fa-envelope-o fa-fw"/>
-                </IconWithBadge>
-        </DropToggle>
-        <ExtendedDropDown right>
-            <ExtendedDropDown.Section className = "d-flex justify-content-between align-items-center">
-                <h4 className="m0"> Message</h4>
-                <ExtendedDropDown.Link to ="app/email">
-                    <i className="fa  fa-pencil"/>
-                </ExtendedDropDown.Link>
-            </ExtendedDropDown.Section>
+  <UncontrolledDropdown nav inNavbar {...props}>
+    <DropToggle nav>
+      <IconWithBadge
+        badge={
+          <Badge pill color="secondary">
+            111
+          </Badge>
+        }
+      >
+        <i className="fa fa-envelope-o fa-fw" />
+      </IconWithBadge>
+    </DropToggle>
+    <ExtendedDropDown right>
+      <ExtendedDropDown.Section className="d-flex justify-content-between align-items-center">
+        <h4 className="m0"> Message</h4>
+        <ExtendedDropDown.Link to="app/email">
+          <i className="fa  fa-pencil" />
+        </ExtendedDropDown.Link>
+      </ExtendedDropDown.Section>
 
-            <ExtendedDropDown.Section list>
-                <ListGroup>
-                    {<Media>
-                        <Media left>
-                            <Avatar.image src = {faker.image.avatar()}
-                            className="avatar-1"/>
-                        </Media>
-                        <Media body>
-                            <span className="d-flex justify-content-start">
-                                <i className={`fa fa-circle small ${messagesColors[index]} mr-2 d-flex align-items-center`}/>
-                            </span>
-                        </Media>
-                    </Media>  
-                    }
-                </ListGroup>
-
-            </ExtendedDropDown.Section>
-        </ExtendedDropDown>
-    </UncontrolledDropdown >
-
-}
+      <ExtendedDropDown.Section list>
+        <ListGroup>
+          {
+            <Media>
+              <Media left>
+                <Avatar.image src={faker.image.avatar()} className="avatar-1" />
+              </Media>
+              <Media body>
+                <span className="d-flex justify-content-start">
+                  <i
+                    className={`fa fa-circle small ${messagesColors[index]} mr-2 d-flex align-items-center`}
+                  />
+                </span>
+              </Media>
+            </Media>
+          }
+        </ListGroup>
+      </ExtendedDropDown.Section>
+    </ExtendedDropDown>
+  </UncontrolledDropdown>;
+};
 NavbarMessages.propTypes = {
-    className: PropTypes.string,
-    style: PropTypes.object
+  className: PropTypes.string,
+  style: PropTypes.object,
 };
 
-export {NavbarMessages};
+export { NavbarMessages };
