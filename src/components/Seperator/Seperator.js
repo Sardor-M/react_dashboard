e45d9035f2012521ab9a3560d46c749ae0b4526c
Sardor Madaminov <sardor0968@gmail.com>
2022-06-16 22:58:54 +0900

    seperator component added

---
+++ src/components/Seperator/Seperator.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
+export const Seperator = ({ position, children, className }) => {
+  const seperatorClass = classNames(
+    {
+      "hr-text-center": position === "center",
+      "hr-text-left": position === "left",
+    },
+    "hr-text",
+    className
+  );
+
+  return <div className={seperatorClass}>{children}</div>;
+};
+
+Seperator.propTypes = {
+  position: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Seperator;
