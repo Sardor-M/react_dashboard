517b385b0bbf2fe184227c2eea81912598ab2fd2
Sardor Madaminov <sardor0968@gmail.com>
2022-06-30 22:47:29 +0900

    index.js : initialized

---
+++ src/components/LayoutUnfilled/index.js
@@ -0,0 +1,6 @@
+import { LayoutUnfilled } from "./LayoutUnfilled";
+import { LayoutUnfilledBar } from "./LayoutUnfilledBar";
+
+LayoutUnfilled.Section = LayoutUnfilledBar;
+
+export default LayoutUnfilled;
