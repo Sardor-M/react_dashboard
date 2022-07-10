25f49eebc408d81fec0450fd17aa55f8b5660793
Sardor Madaminov <sardor0968@gmail.com>
2022-07-11 00:00:02 +0900

    deleted:navbar item

--- src/layout/Sidebar1Navbar.js
+++ src/layout/Sidebar1Navbar.js
@@ -7,14 +7,7 @@ import { NavbarActivityFeed } from "./NavbarActivityFeed";
 
 export const Sidebar1Navbar = () => (
   <Navbar light expand="xs" fluid>
-    <Nav navbar>
-      <NavItem className="m1-3">
-        <SidebarCatch />
-      </NavItem>
-      <NavItem className="navbar-brand h5 mb-0 d-lg-none">
-        <Link to="/">react.bs5</Link>
-      </NavItem>
-    </Nav>
+    <Nav navbar></Nav>
     <Nav navbar className="ml-auto">
       <NavbarActivityFeed />
       <NavbarMessages className="ml-2" />
