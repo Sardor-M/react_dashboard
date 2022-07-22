ee201ba4714b4a7154a9d0cf955a58a94740aa04
Sardor Madaminov <65296404+Sardor-M@users.noreply.github.com>
2022-07-22 10:29:29 +0900

    Provider: initialized

--- src/components/Theme/ThemeProvider.js
+++ src/components/Theme/ThemeProvider.js
@@ -1,1 +1,44 @@
+import React, { Children } from 'react';
+import PropTypes  from 'react';
+import { Provider } from './Theme';
 
+
+export class ThemeProvider extends React.Component {
+    static PropTypes = {
+        children: PropTypes.string,
+        initialStyle: PropTypes.string,
+        initialColor: PropTypes.string,
+    };
+
+    constructor (props) {
+        super(props);
+
+        this.state ={
+            style: "light",
+            color:"primary",
+        };
+    }
+    componentDidMount() {
+        const {initialStyle, initialColor} = this.props;
+
+        if (initialStyle) {
+            this.setState({style: intialStyle});
+        }
+        if (initialColor) {
+            this.setState({color: initialColor});
+        }
+    }
+    onChangeTheme(themeState) {
+        this.setState(themeState);
+    }
+    render () {
+        const {children} = this.props;
+
+        return (
+            <Provider 
+                value={{...this, onChangeTheme:this.onChangeTheme.bind(this)
+            }}>
+                {children}
+            </Provider>
+        );
+  
