6ea4db64ae674bd6fccd7278a66ef157b2dd7cf8
Sardor Madaminov <65296404+Sardor-M@users.noreply.github.com>
2022-07-22 10:41:14 +0900

    themeSelector: added

--- src/components/Theme/ThemeSelect.js
+++ src/components/Theme/ThemeSelect.js
@@ -1,1 +1,41 @@
+import React from 'react';
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import {
+    Card, 
+    CardInside,
+    Button,
+    CustomInput
+} from "reactstrap";
 
+import "./../theme-selector.scss";
+import { Customer } from './Theme';
+
+class ThemeSlector extends React.Component {
+    static PropTypes = {
+        style: PropTypes.string.isRequired,
+        color: PropTypes.string.isRequired,
+        styleOptions: PropTypes.array,
+        colorOptions: PropTypes.bool,
+        onChangeTheme: PropTypes.func,
+    };
+
+    static defaultProps = {
+        styleOptions: [
+            { name: 'Light', value: 'light' },
+            { name: 'Dark', value: 'dark' },
+            { name: 'Color', value: 'color' }
+        ],
+        colorOptions: [
+            { name: 'Primary', value: 'primary' },
+            { name: 'Success', value: 'success' },
+            { name: 'Info', value: 'info' },
+            { name: 'Danger', value: 'danger' },
+            { name: 'Warning', value: 'warning' },
+            { name: 'Indigo', value: 'indigo' },
+            { name: 'Purple', value: 'purple' },
+            { name: 'Pink', value: 'pink' },
+            { name: 'Yellow', value: 'yellow' }
+        ]
+    };
+}
