bf7bf1f86af69e7eddb6b36c225b843b4b15a846
Sardor Madaminov <65296404+Sardor-M@users.noreply.github.com>
2022-07-22 10:10:21 +0900

    theme: added

---
+++ src/components/Theme/Theme.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Customer } from "./ThemeClass";
+
+const Theme = ({ children, color, style }) => {
+  const layoutTheme = `layout--theme--$ {style}--$ {color}`;
+  return children(layoutTheme);
+};
+Theme.PropTypes = {
+  chilren: PropTypes.func.isRequired,
+  color: PropTypes.string,
+  style: PropTypes.string,
+};
+
+const ContextTheme = (otherProps) => (
+  <Customer>
+    {(themeState) => <Theme {...{ ...themeState, ...otherProps }} />}
+  </Customer>
+);
+
+export default Theme;
